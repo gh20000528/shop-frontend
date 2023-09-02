0d3b7d72fdbeb9bf5002ecbfa47a400225448b14
han sheng <gh890528@gmail.com>
2023-09-02 10:50:00 +0800

    frontend first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Shop from './pages/Shop'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Shop/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+      <p>hello this is a shop</p>
+    </div>
+  )
+}
+
+export default Shop
